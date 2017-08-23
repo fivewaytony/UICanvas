864b98b87f4113b6112ffee2b17f23fed981bf44
DESKTOP-13KQ965\YOUNG <webky99@gmail.com>
2017-08-24 05:58:39 +0900

    버튼 애니메이션

---
+++ Assets/Scripts/Text.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Text : MonoBehaviour {
+    GameObject hpSilder;
+    GameObject hpText;
+
+   Text text;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ViewSilderValue()
+    {
+        hpSilder = GameObject.Find("HPSilder");
+        hpText = GameObject.Find("HPText");
+        float val;
+        val = hpSilder.GetComponent<Slider>().value;
+     //   this.GetComponent<Text>().text = "12134";
+     //   hpText.GetComponent<Text>().text = hpSilder.GetComponent<Slider>().value.ToString();
+
+    }
+}
